24b1f20a1676f83e7147826ba73aebfe0a374699
RageDoninelli <ragesamurai194@gmail.com>
2026-09-18 04:03:41 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = "viva-kids-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const isHTML = e.request.mode === "navigate" || e.request.destination === "document";
+  if (isHTML) {
+    // network-first for HTML so updates always show
+    e.respondWith(
+      fetch(e.request)
+        .then((res) => {
+          caches.open(CACHE).then((c) => c.put(e.request, res.clone()));
+          return res;
+        })
+        .catch(() => caches.match(e.request).then((r) => r || caches.match("./index.html")))
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then((cached) => cached || fetch(e.request))
+    );
+  }
+});
